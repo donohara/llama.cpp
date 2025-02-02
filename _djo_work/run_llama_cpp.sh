ed04ccec667c9a2ab1420230cfd7cc5202213a17
Don O'Hara <don.ohara@gmail.com>
2025-02-02 16:04:56 -0500

    d2025-0202_a

--- _djo_work/run_llama_cpp.sh
+++ _djo_work/run_llama_cpp.sh
@@ -6,7 +6,7 @@ PROJECT_DIR="/Users/donohara/Data/50_HU/_github/llama.cpp"
 LLAMA_CPP="$PROJECT_DIR/build-mac-optimized-debug/bin/llama-server"
 
 #CMD="$LLAMA_CPP --model $MODEL_DIR/$MODEL_NAME --prompt \"Hello, world!\" --n-predict 100 --ctx_size 256 --threads 1"
-CMD="$LLAMA_CPP --model $MODEL_DIR/$MODEL_NAME --n-predict 100 --ctx_size 2048 --threads 8 --verbose-prompt"
+CMD="$LLAMA_CPP --model $MODEL_DIR/$MODEL_NAME --n-predict 1024 --ctx_size 4096 --threads 32 --verbose-prompt"
 
 echo $CMD
 eval $CMD
